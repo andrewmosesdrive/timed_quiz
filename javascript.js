22022f8c4024693c3a2f4502acfcb3e97dd5d963
Andrew Moses <andrewmosesdrive@gmail.com>
2020-09-04 17:54:37 -0700

    scrapped old structure; too complicated and was storing html in js. restructured in simpler way, basic functionality of quiz set

--- javascript.js
+++ javascript.js
@@ -1,88 +1,160 @@
-// Global constants
-const takeQuizButton = document.querySelector(".take-the-quiz");
-const viewScoresButton = document.querySelector(".scoreboard");
-const quizContainer = document.querySelector("#quiz");
-const resultsContainer = document.querySelector("#results");
-const submitButton = document.querySelector("#submit");
-const previousButton = document.querySelector("#previous");
-const nextButton = document.querySelector("#next");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-// Initial array for all questions
-const questionsArray = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+// Quiz structure
 
+// Array of questions with question, answer options, and correct answer in each object
+const questionArray = [
+    {
+        question: "blah",
+        a: "okay",
+        b: "nah",
+        c: "duh",
+        d: "yup",
+        correct: "c",
+    },
 
-// Function that only executes "takeQuiz" quiz once navbar button is pressed
-function takeQuiz() {
+    {
+        question: "yeet",
+        a: "okay",
+        b: "nah",
+        c: "duh",
+        d: "yup",
+        correct: "d",
+
+    },
+
+    {
+        question: "lit",
+        a: "okay",
+        b: "nah",
+        c: "duh",
+        d: "yup",
+        correct: "a",
+
+    },
+
+    {
+        question: "whoa",
+        a: "okay",
+        b: "nah",
+        c: "duh",
+        d: "yup",
+        correct: "b",
+
+    },
+
+    {
+        question: "neato",
+        a: "okay",
+        b: "nah",
+        c: "duh",
+        d: "yup",
+        correct: "c",
+
+    },
+
+];
+
+// constants
+// note: must use getElementById for corresponding radio buttons in HTML; querySelector will return null
+const quiz = document.getElementById("quiz")
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question");
+const submitBtn = document.getElementById("submit");
+
+// set "currentQuiz" and "score both to 0"
+let currentQuestion = 0;
+let score = 0;
+
+// call primary function "loadQuiz"
+loadQuiz();
+
+function loadQuiz() {
+
+    // call function "deselctAnswers" to deselect answers previously clicked
+    deselectAnswers();
+
+    // set "currentQuizData" to the value of the current quiz within the "quizData" array 
+    const currentQuestionData = questionArray[currentQuestion];
+
+    // when the quiz loads, set the inner text of the question element to the "question" within the current object
+    questionEl.innerText = currentQuestionData.question;
+    
+    // set the inner text of a-d to the corresponding a-d options of the current object
+    a_text.innerText = currentQuestionData.a;
+    b_text.innerText = currentQuestionData.b;
+    c_text.innerText = currentQuestionData.c;
+    d_text.innerText = currentQuestionData.d;
 
 };
 
-// Function that begins the quiz
-function createQuiz() {
-    // array for storing HTML output
-    const output = [];
+// when "getSelected" is called,  
+function getSelected() {
 
-    // for each question
-    questionsArray.forEach(
-        (currentQuestion, questionNumber) => {
+    // set answer to empty string vs returning undefined, 
+    let answer = "";
 
-            // array for storing possible answers
-            const answers = [];
-
-            // for loop: available answers
-            for (letter in currentQuestion.answers) {
-
-                // add an HTML radio button
-                answers.push(
-                    // need code here, utilize jQuery
-                );
-
-                // add question & answers to output
-                output.push(
-                    //  need code here, utilize jQuery
-                );
-
-            };
+    // then for each answer, if the answer is checked
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
         }
-    );
+    })
 
-    // combine output list into one string
-    quizContainer.innerHTML = output.join("");
+    // return nothing (empty string)
+    return answer;
 };
 
-// Function that shows the answers
-function showResults() {
-  
-};
+// for each answer element, is checked answer false
+function deselectAnswers() {
 
-// Function to convert questions into slides for pagination
-function showSlide() {
-    // If/else if/else statements for showing/hiding slides
-};
+    answerEls.forEach((answerEl) => {
 
-// Show the current slide
-showSlide(currentSlide);
-
-// Function for increment of slide position
-function showNextSlide() {
-    showSlide(currentSlide++)
-};
-
-// Function for decrement of slide position
-function showPreviousSlide() {
-    showSlide(currentSlide--)
-};
-
-// Function to view scoreboard with locally stored scores
-function viewScoreboard() {
-
+        answerEl.checked = false;
+    });
 };
 
 
-// addEventListeners for button clicks
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
-submitButton.addEventListener("click", showResults);
-takeQuizButton.addEventListener("click", takeQuiz);
-viewScoresButton.addEventListener("click", viewScoreboard);
 
+// event listener on click
+submitBtn.addEventListener("click", () => {
+
+    // set answer to "getSelected"
+    const answer = getSelected();
+
+    // if the chosen answer is correct on submit, increase the score
+    if (answer) {
+
+        if (answer === questionArray[currentQuestion].correct) {
+            score++
+        };
+
+        // then go to next question within the array
+        currentQuestion++;
+
+        // if the current quiz question object is less than the length of the array, load quiz
+        if (currentQuestion < questionArray.length) {
+
+            loadQuiz();
+
+          // otherwise,  
+        } else {
+
+            // show how many were correctly answered and create "Reload" button
+            quiz.innerHTML =
+                
+            `<h2>You answered correctly at ${score}/${questionArray.length} questions. </h2>
+
+                <button onclick="location.reload()">Reload</button>
+               
+            `;
+        };
+
+    };
+});
+
+// -----------------------------------------------
+
+// Countdown clock/timer structure
